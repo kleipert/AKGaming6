882e347a8dc3d109a6c725beae65a48feaf5412b
COP-Register <leon_baur@hotmail.de>
2026-05-02 14:27:59 +0200

    [ADD] SoundManager

--- Assets/Scripts/Level/PlayerActivator.cs
+++ Assets/Scripts/Level/PlayerActivator.cs
@@ -6,6 +6,7 @@ public class PlayerActivator : MonoBehaviour
     [SerializeField] GameObject player;
     [SerializeField] CinemachineCamera cinemachineCamera;
     [SerializeField] LightManager lightManager;
+    [SerializeField]AudioListener audioListener;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,9 +22,11 @@ public class PlayerActivator : MonoBehaviour
 
     private void ActivatePlayer()
     {
+        audioListener.enabled = false;
         player.SetActive(true);
         cinemachineCamera.Priority = 0;
         lightManager.SetBpm(120f);
         lightManager.SetDuration(0.2f);
+        SoundManager.Instance.SwitchClip();
     }
 }
---
+++ Assets/Scripts/Level/SoundManager.cs
@@ -0,0 +1,94 @@
+using System;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance { get; private set; }
+    
+    [SerializeField] private AudioClip clipIntro;
+    [SerializeField] private AudioClip clipGame;
+    
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private bool playIntroOnStart = true;
+
+    public event Action OnGameClipFinished;
+    
+    private float gameClipTime = 0f;  
+    private bool isPlayingGameClip = false;
+    private bool gameClipFinished = false;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        audioSource.loop = true;
+    }
+
+    private void Start()
+    {
+        if (playIntroOnStart && clipIntro != null)
+            PlayIntro();
+    }
+
+    private void Update()
+    {
+        if (isPlayingGameClip && !gameClipFinished)
+        {
+            gameClipTime += Time.deltaTime;
+
+            if (gameClipTime >= clipGame.length)
+            {
+                gameClipFinished = true;
+                isPlayingGameClip = false;
+                audioSource.Stop();
+                OnGameClipFinished?.Invoke();
+            }
+        }
+    }
+
+    public void SwitchClip()
+    {
+        if (isPlayingGameClip)
+        {
+            PlayIntro();
+        }
+        else
+        {
+            PlayGame();
+        }
+    }
+
+    public void PlayIntro()
+    {
+        if (clipIntro == null) return;
+
+        audioSource.clip = clipIntro;
+        audioSource.time = 0f;
+        audioSource.Play();
+        isPlayingGameClip = false;
+    }
+
+    public void PlayGame()
+    {
+        if (clipGame == null) return;
+
+        audioSource.clip = clipGame;
+        audioSource.time = Mathf.Clamp(gameClipTime, 0f, clipGame.length - 0.05f);
+        audioSource.Play();
+        isPlayingGameClip = true;
+    }
+    
+    public void ResetManager()
+    {
+        gameClipTime = 0f;
+        gameClipFinished = false;
+        isPlayingGameClip = false;
+    }
+}
