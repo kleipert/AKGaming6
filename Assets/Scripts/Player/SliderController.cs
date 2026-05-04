946fa6f64769e19507dc17d346ad1a1f1a228840
COP-Register <leon_baur@hotmail.de>
2026-05-04 17:04:26 +0200

    [FIX] Latency Issue

--- Assets/Scripts/Player/SliderController.cs
+++ Assets/Scripts/Player/SliderController.cs
@@ -7,24 +7,28 @@ namespace Player
     {
         [SerializeField] private float diveForce = 20f;
         [SerializeField] private float startForce = 40f;
-        [SerializeField] private LayerMask groundLayer;
         [SerializeField] private Rigidbody2D rb;
         [SerializeField] private InputActionAsset inputActions;
-    
-        private bool grounded;
+        
+        [SerializeField] private bool useLatencyCompensation = true;
+
+        private bool diveHeld;
+        private double inputTimestamp;
         private InputAction m_diveAction;
 
-        private void OnEnable()
+        private void Awake()
         {
-            /*
-            inputActions.FindActionMap("Player").Enable();
-            rb.AddForce(Vector2.down * startForce, ForceMode2D.Force);*/    
+            Time.fixedDeltaTime = 1f / 100f;
+
+            inputActions.FindActionMap("Player").Disable();
+            inputActions.FindActionMap("UI").Enable();
+            m_diveAction = InputSystem.actions.FindAction("Dive");
         }
 
         public void BoostThisMFer()
         {
             inputActions.FindActionMap("Player").Enable();
-            rb.AddForce(Vector2.down * startForce, ForceMode2D.Force);
+            rb.AddForce(Vector2.down * startForce, ForceMode2D.Impulse);
         }
 
         private void OnDisable()
@@ -32,18 +36,35 @@ namespace Player
             inputActions.FindActionMap("Player").Disable();
         }
 
-        private void Awake()
+        private void Update()
         {
-            inputActions.FindActionMap("Player").Disable();
-            inputActions.FindActionMap("UI").Enable();
-            m_diveAction = InputSystem.actions.FindAction("Dive");
+            diveHeld = m_diveAction.IsPressed();
+            
+            if (m_diveAction.WasPerformedThisFrame())
+            {
+                inputTimestamp = m_diveAction.activeControl?.device.lastUpdateTime ?? Time.unscaledTimeAsDouble;
+            }
         }
 
         private void FixedUpdate()
         {
-            grounded = Physics2D.CircleCast(transform.position, 0.5f, Vector2.down, 0.1f, groundLayer);
+            if (!diveHeld) return;
 
-            if (m_diveAction.IsPressed())
+            if (useLatencyCompensation)
+            {
+                float timeSinceInput = (float)(Time.fixedUnscaledTimeAsDouble - inputTimestamp);
+
+                if (timeSinceInput < Time.fixedDeltaTime * 2f)
+                {
+                    float compensationFactor = 1f + (timeSinceInput / Time.fixedDeltaTime);
+                    rb.AddForce(Vector2.down * (diveForce * compensationFactor), ForceMode2D.Force);
+                }
+                else
+                {
+                    rb.AddForce(Vector2.down * diveForce, ForceMode2D.Force);
+                }
+            }
+            else
             {
                 rb.AddForce(Vector2.down * diveForce, ForceMode2D.Force);
             }
